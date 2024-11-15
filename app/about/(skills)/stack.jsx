549d91af6c59fb430eb6563ef15afc11669ec683
Akash Kumar <codewithpanda28@gmail.com>
2024-11-15 22:26:15 +0530

    1st project done

---
+++ app/about/(skills)/stack.jsx
@@ -0,0 +1,25 @@
+export default function Stack() {
+  return (
+		<>
+			<h2 className="text-2xl md:text-xl font-normal mb-3 md:tracking-[.3rem] lg:tracking-[.5rem] uppercase ">
+				Language & Framework
+			</h2>
+			<p className="text-gray-500 text-center tracking-widest md:px-5">
+				<span className="text-black font-bold">HTML</span> |{" "}
+				<span className="text-black font-bold">CSS</span> |{" "}
+				<span className="text-black font-bold">Javascript</span> |{" "}
+				<span className="text-black font-bold">Typescript</span> |{" "}
+				<span className="text-black font-bold">React</span> |{" "}
+				<span className="text-black font-bold">NextJS</span> |{" "}
+				<span className="text-black font-bold">TailwindCSS</span> |{" "}
+				<span className="text-black font-bold">Bootstrap</span> |{" "}
+				<span className="text-black font-medium">Styled Components</span> |{" "}
+				<span className="text-black font-medium">Material-UI</span> |{" "}
+				<span className="text-black font-bold">Ant Design</span> |{" "}
+				<span className="text-black font-bold">REST APIs</span> |{" "}
+	
+		
+			</p>
+		</>
+  );
+}
---
+++ app/layout.jsx
@@ -0,0 +1,65 @@
+'use client'
+import dynamic from "next/dynamic";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+// import TopProgressBar from "@/components/TopProgressBar";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+import Chat from "@/components/Chat";
+
+import "./nprogress.css";
+import { Analytics } from "@vercel/analytics/react";
+import TopProgressbar from "@/components/TopProgressbar";
+
+export const Getmetadata = {
+	title: "Akash | Portofolio",
+	description:
+		"My name is Akash, I'm a web developer and I'm passionate about it. I'm currently studying at Universitas Negeri Malang.",
+	author: "Akash Kumar",
+	siteUrl: "https://www.Akash.my.id",
+	applicationName: "Akash",
+	keywords: [
+		"Akash",
+		"Akash",
+		"Akash Kumar",
+	],
+	openGraph: {
+		type: "website",
+		url: "https://www.Akash.my.id",
+		title: "Akash | Portofolio",
+		site_name: "Akash | Portofolio",
+		description: "My name is Akash, This is my portofolio website.",
+		width: 1200,
+		height: 630,
+		images: [
+			{
+				url: "/og-image-rev.png",
+				alt: "Akash Portofolio",
+			},
+		],
+		site_name: "Akash | Portofolio",
+	},
+};
+
+
+
+const TopProgressBar = dynamic(() => import("@/components/TopProgressbar"), {
+	ssr: false,
+});
+
+
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<TopProgressBar />
+				<Navbar />
+				{children}
+				<Chat />
+				<Analytics />
+			</body>
+		</html>
+	);
+}
