8f5b32f90573c9e3989adf528f5ae320031e5d9b
Akash Kumar <codewithpanda28@gmail.com>
2024-11-17 23:48:29 +0530

    some changes in chatbot

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -6,17 +6,7 @@ import { useEffect } from "react";
 const Chat = () => {
 	useEffect(() => {
 		// Paste the Intelliticks script here
-		(function (I, L, T, i, c, k, s) {
-			if (I.iticks) return;
-			I.iticks = { host: c, settings: s, clientId: k, cdn: L, queue: [] };
-			var h = T.head || T.documentElement;
-			var e = T.createElement(i);
-			var l = I.location;
-			e.async = true;
-			e.src = (L || c) + "/client/inject-v2.min.js";
-			h.insertBefore(e, h.firstChild);
-			I.iticks.call = function (a, b) {
-				I.iticks.queue.push([a, b]);
+		(function(I, L, T, i, c, k, s) {if(I.iticks) return;I.iticks = {host:c, settings:s, clientId:k, cdn:L, queue:[]};var h = T.head || T.documentElement;var e = T.createElement(i);var l = I.location;e.async = true;e.src = (L||c)+'/client/inject-v2.min.js';h.insertBefore(e, h.firstChild);I.iticks.call = function(a, b) {I.iticks.queue.push([a, b]);
 			};
 		})
 		(
